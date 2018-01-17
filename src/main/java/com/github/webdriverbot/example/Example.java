24b19bda2b44b36eafc0d44fa20340f55e8142a5
xinaiz <dominic.jurczak@yahoo.com>
2018-01-17 14:19:07 -0800

    simplified example file

--- src/main/java/com/github/webdriverbot/example/Example.java
+++ src/main/java/com/github/webdriverbot/example/Example.java
@@ -7,36 +7,26 @@ import org.openqa.selenium.WebDriver;
 import com.github.webdriverextensions.WebDriverExtensionsContext;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
 public class Example {
 
     public static void main(String[] args) {
 
-        ExecutorService executor = Executors.newCachedThreadPool();
-        for (int i = 0; i < 3; ++i) {
-            executor.execute(() -> {
-                init();
+        init();
 
-                GoogleSearchPage googleSearchPage = openPage(GoogleSearchPage.class);
-                type("pizza", googleSearchPage.searchField);
-                click(GoogleSearchPage_.submitButton.class);
+        GoogleSearchPage googleSearchPage = openPage(GoogleSearchPage.class);
+        type("pizza", googleSearchPage.searchField);
+        click(GoogleSearchPage_.submitButton.class);
 
-                List<String> urls = new ArrayList<>();
-                forEachRedirect(GoogleSearchResults_.links.class, () -> {
-                    urls.add(currentUrl());
-                });
+        List<String> urls = new ArrayList<>();
+        forEachRedirect(GoogleSearchResults_.links.class, () -> {
+            urls.add(currentUrl());
+        });
 
-                for (String url : urls) {
-                    System.out.println("url: " + url);
-                }
+        System.out.println("Found urls: " + urls);
 
-                driver().quit();
-            });
-        }
+        driver().quit();
 
     }
 
