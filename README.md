bd56d2d3f44683e706b860037241bb8096a1607d
xinaiz <dominic.jurczak@yahoo.com>
2018-01-17 06:50:48 -0800

    released version 1.0.0

--- README.md
+++
@@ -1,2 +0,0 @@
-# webdriverbot
-Extension for WebDriver project
--- src/main/java/com/github/webdriverbot/FieldDecoratorFactory.java
+++
@@ -1,9 +0,0 @@
-package com.github.webdriverbot;
-
-import org.openqa.selenium.WebDriver;
-
-public interface FieldDecoratorFactory {
-
-    public WebDriverBotFieldDecorator create(WebDriver drivera);
-    
-}
